0acbce24d5d5ff27445d55ed7f2403de6aac76d4
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-14 17:25:38 +0100

    FEAT 2 col, data, v-for and reactive percentage

---
+++ src/components/Leader.vue
@@ -0,0 +1,135 @@
+<script>
+export default {
+    data() {
+        return {
+            Percentage: [
+                {
+                    title_perc: 'mentorship',
+                    number_perc: '78%'
+                },
+                {
+                    title_perc: 'education',
+                    number_perc: '95%'
+                },
+                {
+                    title_perc: 'learning',
+                    number_perc: '65%'
+                },
+                {
+                    title_perc: 'motivation',
+                    number_perc: '83%'
+                }
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col section-left">
+                <div class="leader">
+                    <h1 class="title">creative leader</h1>
+                    <span class="point">.</span>
+                </div>
+                <span class="line"></span>
+                <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim numquam laboriosam optio!
+                    Vitae accusamus deleniti, fugiat temporibus laudantium nobis consectetur dolores? Ab, possimus nostrum.
+                    Id quam optio totam. Sequi odit obcaecati, illo possimus autem tenetur rem?
+                    Delectus iure error consequuntur sapiente odio magnam eos. Quod enim obcaecati possimus.</p>
+                <button class="btn">read more</button>
+            </div>
+            <div class="col">
+                <div v-for="el in Percentage">
+                    <div class="percentuale">
+                        <h2 class="title_perc">{{ el.title_perc }}</h2>
+                        <span class="number_perc">{{ el.number_perc }}</span>
+                    </div>
+                    <span class="loader" :style="{ width: el.number_perc }"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.row {
+    display: flex;
+    flex-direction: row;
+    max-width: 1380px;
+    margin: 0 auto;
+    gap: 7.5rem;
+}
+
+.col {
+    display: flex;
+    flex-direction: column;
+    flex-basis: 50%;
+}
+
+.leader {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+}
+
+.title {
+    text-transform: capitalize;
+    font-size: 2.5rem;
+    margin: 0;
+}
+
+.point {
+    color: #ff4612;
+    font-size: 2.5rem;
+}
+
+.line {
+    border-bottom: 2px solid #ff4612;
+    display: inline-block;
+    width: 9%;
+}
+
+.text {
+    font-size: 1rem;
+    font-weight: 300;
+    font-family: "Source Sans Pro", sans-serif;
+    line-height: 24px;
+}
+
+.btn {
+    border: none;
+    background-color: transparent;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 11px;
+    text-align: start;
+}
+
+.percentuale {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+    margin-bottom: -1rem;
+}
+
+.title_perc {
+    text-transform: capitalize;
+    font-size: 1.2rem;
+    margin: 0;
+    padding: 1.8rem 0;
+}
+
+.number_perc {
+    font-weight: bold;
+    font-size: 1.2rem;
+}
+
+.loader {
+    display: inline-block;
+    background-color: #ff4612;
+    height: 3px;
+    // width: 100px;
+}
+</style>
