bb2879fac6953047e107107d124613e0b223a4f2
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-14 09:01:05 +0100

    FEAT create container bickford part

---
+++ src/components/Bickford.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="/img/h1-img-01.jpg" alt="">
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped></style>
