e49860a425e3960135429becfef4aeb35f79f101
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-15 13:13:10 +0100

    FEAT component copyright

---
+++ src/components/Copyright.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <p>copyright everlead &copy; mikadoThemes </p>
+</template>
+<style lang="scss" scoped>
+p {
+    color: white;
+    font-size: 15px;
+    font-weight: lighter;
+    text-align: center;
+    width: 100%;
+    text-transform: capitalize;
+    padding: 20px 0;
+}
+</style>
