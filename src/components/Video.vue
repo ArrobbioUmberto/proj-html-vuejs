2b47019f951dd105e6bf3244d2c6117dc6d09610
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-15 10:47:54 +0100

    FEAT video components

---
+++ src/components/Video.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <i class="fa-solid fa-play"></i>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+i {
+    color: white;
+    font-size: 60px;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+
+.col {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
